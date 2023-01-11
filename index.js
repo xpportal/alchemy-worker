560f0d7ed535b479294fb51930883de46e342a1e
antpb <itartist.pdf@gmail.com>
2023-01-11 02:35:48 -0600

    revises worker to allow requests from remote

--- index.js
+++ index.js
@@ -1,61 +1,76 @@
-export default {
-	async fetch(request, env) {
-	  return await handleRequest(request)
-	}
-  }
-  
-  async function handleRequest(request) {
-	// Extract the bearer token from the request headers
-	const authorization = request.headers.get('Authorization');
-	if (!authorization) {
-	  return new Response('Unauthorized', { status: 401 });
-	}
+async function handleRequest(request) {
+    if (request.method === 'OPTIONS') {
+        return handleOptions(request);
+    } else {
+        return handleFetch(request);
+    }
+}
+
+async function handleFetch(request) {
+    // Extract the bearer token from the request headers
+    const authorization = request.headers.get('Authorization');
+    if (!authorization) {
+        return new Response('Unauthorized', { status: 401 });
+    }
+
+    // Process the body of the request
     const body = await request.text();
     const data = JSON.parse(body);
     const input = data.input;
-	// Needs to be a proper json string.
-	let prompt = data.personality;
-	let finalPrompt = prompt
-		.replaceAll('#speaker', data.Speaker)
-		.replaceAll('#input', data.Input)
-		.replaceAll('#agent', data.Agent)
-		.replaceAll('#conversation', data.Conversation)
-		.replaceAll('undefined\n','' ).replaceAll('undefined','')
-		.slice(-5000)
+    // Needs to be a proper json string.
+    let prompt = data.personality;
+    let finalPrompt = prompt
+        .replaceAll('#speaker', data.Speaker)
+        .replaceAll('#input', data.Input)
+        .replaceAll('#agent', data.Agent)
+        .replaceAll('#conversation', data.Conversation)
+        .replaceAll('undefined\n','' ).replaceAll('undefined','')
+        .slice(-5000)
 
-	const token = authorization.split(' ')[1];
-	const postData = {
-		prompt: finalPrompt	,
-		max_tokens: 500,
-		stop : ["###"],
-	  };
-	// Make the first request to the Davinci model
-	const davinciResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-	  method: 'POST',
-	  headers: {
-		'Content-Type': 'application/json',
-		'Authorization': `Bearer ${token}` // use the bearer token from the request
-	  },
-	  body: JSON.stringify(postData)
-	});
-  
-	// Process the response from the Davinci model
-	const davinciData = await davinciResponse.json();
-	console.log(davinciData);
-  
-	const headers = {
-	  'Access-Control-Allow-Origin': '*',
-	  'Content-Type': 'application/json',
-	  "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
-	  "Access-Control-Allow-Headers": "*",
-	  "Access-Control-Allow-Credentials": "true"
-  
-	};
-  
-	// Return the results to the client
-	return new Response(JSON.stringify({ davinciData }), {
-	  headers
-	});
-  }
-  
-  
+    const token = authorization.split(' ')[1];
+    const postData = {
+        prompt: finalPrompt	,
+        max_tokens: 500,
+        stop : ["###"],
+    };
+
+    // Make the first request to the Davinci model
+    const davinciResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(postData)
+    });
+
+    // Process the response from the Davinci model
+    const davinciData = await davinciResponse.json();
+    console.log(davinciData);
+
+    // Return the results to the client
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    };
+    return new Response(JSON.stringify({ davinciData }), {
+        headers
+    });
+}
+
+function handleOptions(request) {
+    // Make sure the necessary headers are present
+    // for this to be a valid pre-flight request
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    };
+    return new Response(null, { headers });
+}
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+});
